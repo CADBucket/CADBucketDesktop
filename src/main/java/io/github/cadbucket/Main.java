acda3945ed8c3cba1427c499a08047c01d95a0a2
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2023-01-08 21:44:20 -0600

    Add window

--- src/main/java/io/github/cadbucket/Main.java
+++ src/main/java/io/github/cadbucket/Main.java
@@ -2,7 +2,7 @@ package io.github.cadbucket;
 
 public final class Main {
     public static void main(String[] args) {
-        System.out.println("Project works");
+        MainWindow window = new MainWindow();
     }
 
     private Main() {
