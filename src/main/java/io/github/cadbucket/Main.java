d9202eda26f13c163fd7b23b5eb6d4724dbc2da6
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2023-01-08 21:37:13 -0600

    Add project

---
+++ src/main/java/io/github/cadbucket/Main.java
@@ -0,0 +1,11 @@
+package io.github.cadbucket;
+
+public final class Main {
+    public static void main(String[] args) {
+        System.out.println("Project works");
+    }
+
+    private Main() {
+        throw new AssertionError();
+    }
+}
